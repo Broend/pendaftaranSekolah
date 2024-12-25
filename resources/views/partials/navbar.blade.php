936c4defd3c53b1c7bb0cc719cbdd6b0035988da
Fadli1009 <argyfa3@gmail.com>
2024-12-25 10:33:12 +0700

    menambahkan navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="flex justify-between items-center px-5 lg:px-[75px] bg-gradient-to-r from-indigo-500 to-indigo-400">
+    <a href="" class="flex items-center space-x-4">
+        <img src="https://satujam.com/wp-content/uploads/2017/08/20150622033653LOGO_OSIS_SMA.jpg"
+            class="w-[70px] py-5 rounded" alt="">
+        <h1 class="text-white font-bold text-xl">SMA 1123 Kudung Ngidul</h1>
+    </a>
+    <div>
+        <ul class="flex space-x-4 text-white">
+            <li class="hover:text-gray-200 transition"><a href="">Dashboard</a></li>
+            <li class="hover:text-gray-200 transition"><a href="">Profil</a></li>
+            <li class="hover:text-gray-200 transition"><a href="">Contact</a></li>
+            <li class="hover:text-gray-200 transition"><a href="">Logout</a></li>
+        </ul>
+    </div>
+</nav>
