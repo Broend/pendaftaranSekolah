82b61d1abbedf9f5aecf6e61c105aa9bd142d21a
Fadli1009 <argyfa3@gmail.com>
2024-12-25 12:28:38 +0700

    cek

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::resource('students', StudetsController::class);
-//dwadwadawdwa
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'loginAction'])->name('login');
 Route::get('/register', [AuthController::class, 'register']);
